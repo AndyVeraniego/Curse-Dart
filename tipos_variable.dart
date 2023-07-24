c70e88eece7d5e18516c6ec8b018b2f5b4810822
AndyVeraniego <140358773+AndyVeraniego@users.noreply.github.com>
2023-07-23 21:57:03 -0400

    Add files via upload

---
+++ tipos_variable.dart
@@ -0,0 +1,16 @@
+void main() {
+  final String nombre = 'pedro';
+  final int numero = 100;
+  final bool boleano= true;
+  final List<String> abilities = ['impostor'];
+  final sprites = <String>['Andy','Cami','Cebra','Peluche'];
+
+  print("""
+  $nombre
+  $numero
+  $boleano
+  $abilities
+  $sprites
+
+"""); 
+}
